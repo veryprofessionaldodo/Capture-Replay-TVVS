064245411d7210c27cf77e062af7a62e27db2ae2
veryprofessionaldodo <pedroreis.ad@gmail.com>
2019-10-20 19:10:30 +0100

    Added template for exercises

---
+++ CalculatorWeb/ex1.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, url_for
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('calculator1.html')
+
+if __name__ == '__main__':
+    app.run(debug=False, port=int(os.getenv('PORT', 1111)))
--- CalculatorWeb/ex2.py
+++ CalculatorWeb/ex2.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, url_for
 import socket
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('calculator2.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, port=int(os.getenv('PORT', 1111)))
